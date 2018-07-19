bc3ef4f2060a074a661bbad4b4146c6a557ae0c0
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2018-07-19 22:36:05 +0900

    呼び出し追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SampleLib1Module } from 'sample-lib1';
+import { SampleLib2Module } from 'sample-lib2';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SampleLib1Module,
+    SampleLib2Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
